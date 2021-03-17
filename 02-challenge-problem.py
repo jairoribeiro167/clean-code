c7f7bf4518d312658eaf34ff5ba72fd63d84dc59
Jairo Ribeiro <jairoduarte123@gmail.com>
2021-03-17 09:46:51 -0300

    Refactor code removing comments and fixing vertical and horizontal spacing

--- 02-challenge-problem.py
+++ 02-challenge-problem.py
@@ -1,15 +1,7 @@
 # (c) Maximilian Schwarzmüller / Academind GmbH
 
-# *********
-# Imports
-# *********
 from os import path, makedirs
 from pathlib import Path
-
-# *********
-# Main
-# *********
-# A class which allows us to create DiskStorage instances
 
 
 class DiskStorage:
@@ -19,19 +11,20 @@ class DiskStorage:
     def get_directory_path(self):
         return Path(self.storage_directory)
 
-    # This must be called before a file is inserted
     def create_directory(self):
         if (not path.exists(self.get_directory_path())):
             makedirs(self.storage_directory)
 
     # Warning: Directory must exist in advance
     def insert_file(self, file_name, content):
-        file = open(self.get_directory_path() / file_name, 'w')
+        file_path = self.get_directory_path() / file_name
+        file = open(file_path, 'w')
         file.write(content)
         file.close()
-        # Todo: Add proper error handling
+        # TODO: Add proper error handling
 
 
 log_storage = DiskStorage('logs')
 
+log_storage.create_directory()
 log_storage.insert_file('test.txt', 'Test')
